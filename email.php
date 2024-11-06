a7dff58b8d383c2499082e2e2b94ead586110bbe
VictorMokoatsi <mokoatsivictor950416@gmail.com>
2024-11-06 10:59:14 +0200

    Update email.php

--- email.php
+++ email.php
@@ -4,45 +4,45 @@
     $full_name = $_POST['full_name'];
     $email = $_POST['email'];
     $phone_number = $_POST['phone_number'];
-    // $subject = $_POST['subject'];
     $msg = $_POST['msg'];
+    $subject = $_POST['subject'];
     // $receiver_email = $_POST['$receiver_email']
 
-    echo "Company Name : $company_name <br>";
-    echo "Last Name : $lname <br>";
+    echo "Full Names : $full_name <br>";
     echo "Email address : $email <br>";
-    echo "Phone Number : $p_number <br>";
+    echo "Phone Number : $phone_number <br>";
+    echo "Subject: $subject <br>";
     echo "Topic : $msg <br>";
 
     
     // Function to generate a random OTP
-    function generateOTP($length = 4)
-    {
-        $characters = '0123456789';
-        $otp = '';
-        for ($i = 0; $i < $length; $i++) {
-            $otp .= $characters[rand(0, strlen($characters) - 1)];
-        }
-        return $otp;
-    }
+    // function generateOTP($length = 4)
+    // {
+    //     $characters = '0123456789';
+    //     $otp = '';
+    //     for ($i = 0; $i < $length; $i++) {
+    //         $otp .= $characters[rand(0, strlen($characters) - 1)];
+    //     }
+    //     return $otp;
+    // }
     
     // Generate OTP
-    $otp = generateOTP();
+    // $otp = generateOTP();
     
     //It display the sender's name and email address into the receiver email.
     // $mail_header = "From: " . $email . "\r\n";
-    $mail_header = "From : mokoatsivictor950416@gmail.com";
+    $receiver = "To : mokoatsivictor@gmail.com";
     
-    $subject = "Your OTP is";
+    // $subject = "Your OTP is";
 
     //Enter your email adderss to receive the messages sent to you from $mail_header
     // $receiver = "lerato@abtworld.co.za";
-    $receiver = $email . "\r\n";
+    $mail_header ="From: " . $email . "\r\n";
     
-    $message = "Do not share this code with anyone. To finish logging into your zarfin account, enter this code: $otp";
+    // $message = "Do not share this code with anyone. To finish logging into your zarfin account, enter this code: $otp";
     
 
-    mail($receiver, $mail_header, $message, $subject)
+    mail($receiver, $mail_header, $subject)
     or die("error 1");
 
     echo '
@@ -57,7 +57,7 @@
 
                     <div class="container">
                         <h1>Thank you for contacting me.</h1>
-                        <p class="back">Go back to the <a href="index.html">Contact page</a>.</p>
+                        <p class="back">Go back to the <a href="index.php">Contact page</a>.</p>
 
                         </div>
                     </div>
